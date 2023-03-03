321bbc7b79499c533aae2f49a461318dd0a842c2
vahabbayat <vahabbayat@gmail.com>
2023-03-03 01:18:40 -0500

    modify

--- move/setup.py
+++ move/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'edge_follower = move.edge_follower:main'
+            'edge_follower = move.edge_follower:main',
         ],
     },
 )
