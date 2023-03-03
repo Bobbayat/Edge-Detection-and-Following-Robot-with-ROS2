7aa50f683ffce0841b3fbe83c6e33a0b1eaf016d
vahabbayat <vahabbayat@gmail.com>
2023-03-03 02:01:41 -0500

    midified

--- move/move/move.py
+++ move/move/move.py
@@ -26,44 +26,43 @@ class Move(Node):
         else:
             # Convert the image to grayscale and apply Canny edge detection
             gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+            edges = cv2.Canny(gray, 100, 200)
+
+            # Determine the center of the image
+            height, width = edges.shape
+            center = int(width / 2)
+
+            # Find the edge pixels
+            edge_pixels = []
+            for i in range(height):
+                for j in range(width):
+                    if edges[i, j] == 255:
+                        edge_pixels.append((j, i))
+
+            # If no edge pixels are found, stop moving
+            if len(edge_pixels) == 0:
+                msg = Twist()
+                msg.direction = "stop"
+                self.vel_publisher.publish(msg)
         
-        edges = cv2.Canny(gray, 100, 200)
 
-        # Determine the center of the image
-        height, width = edges.shape
-        center = int(width / 2)
+            # Find the x-coordinate of the closest edge pixel to the center of the image
+            closest_pixel = min(edge_pixels, key=lambda p: abs(p[0] - center))
+            closest_x = closest_pixel[0]
 
-        # Find the edge pixels
-        edge_pixels = []
-        for i in range(height):
-            for j in range(width):
-                if edges[i, j] == 255:
-                    edge_pixels.append((j, i))
-
-        # If no edge pixels are found, stop moving
-        if len(edge_pixels) == 0:
-            msg = Twist()
-            msg.direction = "stop"
-            self.vel_publisher.publish(msg)
-        
-
-        # Find the x-coordinate of the closest edge pixel to the center of the image
-        closest_pixel = min(edge_pixels, key=lambda p: abs(p[0] - center))
-        closest_x = closest_pixel[0]
-
-        # Determine the direction to move based on the x-coordinate of the closest edge pixel
-        if closest_x < center - 50:
-            msg = Twist()
-            msg.direction = "left"
-            self.vel_publisher.publish(msg)
-        elif closest_x > center + 50:
-            msg = Twist()
-            msg.direction = "right"
-            self.vel_publisher.publish(msg)
-        else:
-            msg = Twist()
-            msg.direction = "forward"
-            self.vel_publisher.publish(msg)
+            # Determine the direction to move based on the x-coordinate of the closest edge pixel
+            if closest_x < center - 50:
+                msg = Twist()
+                msg.direction = "left"
+                self.vel_publisher.publish(msg)
+            elif closest_x > center + 50:
+                msg = Twist()
+                msg.direction = "right"
+                self.vel_publisher.publish(msg)
+            else:
+                msg = Twist()
+                msg.direction = "forward"
+                self.vel_publisher.publish(msg)
 
 
 def main(args=None):
